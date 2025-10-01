f32a79d413096e4ce9f9d5eeccf6bbaed5c3d61c
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-10-01 14:22:28 +0200

    separate button components

---
+++ resources/js/components/AddCardButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Button from './ui/button/Button.vue';
+
+</script>
+
+<template>
+    <Button class="flex-1 bg-primary text-primary-foreground hover:bg-primary/90 px-6 py-3 rounded-lg font-semibold transition-colors duration-200 hover:cursor-pointer">
+        Add to Cart
+    </Button>
+</template>
